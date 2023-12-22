fa4431d7a964633b1f469b9bd6e2b8048db4bbcb
Neerajkumarc <chaudharyneeraj91@gmail.com>
2023-12-22 18:20:19 +0530

    cleanup

--- script.js
+++ script.js
@@ -1,95 +1,89 @@
-document.getElementById("playBtn").addEventListener("click", () => {
-  document.getElementById("homeDiv").style.display = "none";
-  document.getElementById("gameDiv").style.display = "block";
-  getFlags();
-});
+const playBtn = document.getElementById("playBtn");
+const homeDiv = document.getElementById("homeDiv");
+const gameDiv = document.getElementById("gameDiv");
+const soundCheck = document.getElementById("sound");
+const flagImage = document.getElementById("flag");
+const optionsContainer = document.getElementById("options");
+const highscoreElement = document.getElementById("highscore");
+const scoreElement = document.getElementById("score");
 
-const check = document.getElementById("sound");
-check.addEventListener("change", () => {
-  if (check.checked) {
-    enableSound = true;
-  } else {
-    enableSound = false;
-  }
-});
 let enableSound = false;
-var fwc = [{}];
+let fwc = [];
 let random;
 let score = 0;
 let highscore = localStorage.getItem("highscore") || 0;
+
 const correctAudio = new Audio("./sounds/correct.mp3");
 const incorrectAudio = new Audio("./sounds/incorrect.mp3");
 
-document.getElementById("highscore").textContent = highscore;
+highscoreElement.textContent = highscore;
 
-function updatescore() {
-  score < 0
-    ? (score = 0)
-    : (document.getElementById("score").textContent = score);
+playBtn.addEventListener("click", () => {
+  homeDiv.style.display = "none";
+  gameDiv.style.display = "block";
+  getFlags();
+});
+
+soundCheck.addEventListener("change", () => {
+  enableSound = soundCheck.checked;
+});
+
+function updateScore() {
+  score = Math.max(0, score);
+  scoreElement.textContent = score;
 }
 
 function getFlags() {
-  let optionsArr = [];
+  optionsContainer.innerHTML = "";
   fetch("https://restcountries.com/v3.1/all")
-    .then((res) => res.json())
-    .then((data) => {
-      for (let i = 0; i < data.length; i++) {
-        fwc.push({ country: data[i].name.common, flag: data[i].flags.png });
-      }
-      random = Math.floor(Math.random() * fwc.length + 1);
-      document.getElementById("flag").src = fwc[random].flag;
-      for (let i = 1; i <= 4; i++) {
-        let randomOpt = Math.floor(Math.random() * fwc.length + 1);
-        optionsArr.push(fwc[randomOpt].country);
-      }
-      optionsArr[Math.floor(Math.random() * optionsArr.length)] =
-        fwc[random].country;
+    .then(res => res.json())
+    .then(data => {
+      fwc = data.map(item => ({ country: item.name.common, flag: item.flags.png }));
+      random = Math.floor(Math.random() * fwc.length);
+      flagImage.src = fwc[random].flag;
 
-      optionsArr.forEach((options) => {
+      const optionsArr = Array.from({ length: 4 }, () => {
+        const randomOpt = Math.floor(Math.random() * fwc.length);
+        return fwc[randomOpt].country;
+      });
+
+      optionsArr[Math.floor(Math.random() * optionsArr.length)] = fwc[random].country;
+
+      optionsArr.forEach(option => {
         const optionEl = document.createElement("button");
         optionEl.classList.add("optionsEl");
-        optionEl.innerText = options;
-        document.getElementById("options").appendChild(optionEl);
-        optionEl.addEventListener("click", () => {
-          handleClick(options, optionEl);
-        });
+        optionEl.innerText = option;
+        optionsContainer.appendChild(optionEl);
+        optionEl.addEventListener("click", () => handleClick(option, optionEl));
       });
     });
 }
 
-function playSound(audio){
-  if (audio.paused) {
-    audio.play();
-  } else {
-    audio.currentTime = 0;
-  }
+function playSound(audio) {
+  audio.paused ? audio.play() : (audio.currentTime = 0);
 }
 
-function handleClick(options, optionEl) {
-  if (options == fwc[random].country) {
-    if (enableSound) {
-      playSound(correctAudio)
-    }
+function handleClick(option, optionEl) {
+  if (option === fwc[random].country) {
+    if (enableSound) playSound(correctAudio);
     optionEl.style.border = "4px solid green";
     score += 20;
-    updatescore();
+    updateScore();
     setTimeout(() => {
-      document.getElementById("options").innerHTML = null;
+      optionsContainer.innerHTML = "";
       getFlags();
     }, 1000);
   } else {
-    if (enableSound) {
-      playSound(incorrectAudio)
-    }
+    if (enableSound) playSound(incorrectAudio);
     optionEl.style.border = "4px solid red";
     optionEl.classList.add("animate-error");
     score -= 20;
-    updatescore();
+    updateScore();
   }
 
   if (highscore < score) {
     highscore = score;
     localStorage.setItem("highscore", highscore);
-    document.getElementById("highscore").textContent = highscore;
+    highscoreElement.textContent = highscore;
   }
 }
